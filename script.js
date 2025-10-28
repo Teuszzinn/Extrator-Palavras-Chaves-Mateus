64334ede11272f1f8d6120bc704fb1302683b52e
Teuszzinn <carvalho.leao@escola.pr.gov.br>
2025-10-28 13:47:09 +0000

    656

--- script.js
+++ script.js
@@ -2,17 +2,43 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
-const texto = document.querySelector("#entrada-de-texto").value;
-const campoResultado = document.querySelector("#resultado-palavrachave");
-const palavrasChave = processaTexto(texto);
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
-campoResultado.textContent = palavrasChave.join(", ")
+    campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
-let palavras = texto.split(/\P{L}+/u);
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
 
-return palavras;
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(oredernaPalavra)
+
+    function oredernaPalavra(p1, p2) {
+        return frequencia[p2] - frequencias[p1]
+
+    }
+    return ordenadas.slice(0.10)
+}
+funtion contaFrequencias(palavras){
+
+    let frequencias = {};
+
+    for (let i of palavras) {
+        frequencias[i] = 0;
+
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias(i)++;
+            }
+
+        }
+
+    }
+
+    return palavras;
+
 
 }
