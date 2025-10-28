9f5b1dc1fc5e76740df7754164514e05d5ef1510
Teuszzinn <carvalho.leao@escola.pr.gov.br>
2025-10-28 12:36:08 +0000

    k

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", MostraPalavrasChave);
 
-function MostraPalavrasChave() {
+function MostraPalavrasChave(){
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
-    const palavras = processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
     campoResultado.textContent = palavrasChave.join(", ")
 }
@@ -69,3 +69,5 @@ return palavra;
 
 
 
+ocessaTexto(texto){
+    let palavra = texto.spl
