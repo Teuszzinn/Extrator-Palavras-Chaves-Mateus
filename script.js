4d4a9612e2addd760d7bcf1784b9d076cb0ca777
Teuszzinn <carvalho.leao@escola.pr.gov.br>
2025-11-04 12:38:04 +0000

    28

--- script.js
+++ script.js
@@ -13,8 +13,19 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+
+    palavras = tiraPalavrasRuins(palavras);
+    
     const frequencias = contaFrequencias(palavras);
+
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
+
+
 
     function ordenaPalavras(p1, p2) {
         return frequencias[p2] - frequencias[p1]
